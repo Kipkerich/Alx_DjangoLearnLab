c38616127049c76484ef2db953cef47fd4dbc55a
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-07-13 20:17:40 +0300

    Added list filter configuration

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,4 +4,4 @@ from .models import Book
 admin.site.register(Book)
 
 class BookAdmin(admin.ModelAdmin):
-    list_filter = ('title', 'author', 'Publication_year')
+    list_filter = ('title', 'author', 'publication_year')
