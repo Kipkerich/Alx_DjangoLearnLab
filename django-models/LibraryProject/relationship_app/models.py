dd7b05d329361143bd7bc1cc2693d19b02aae2d9
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-07-21 20:59:27 +0300

    modified the author model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Author(models.Model):
-    author =models.CharField(max_length=100)
+    name =models.CharField(max_length=100)
     
     
 class Book(models.Model):
