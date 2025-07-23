e83f7d391d2e77fa00e48f3bd2bc77ef5a14d106
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-07-23 06:10:38 +0300

    Added function based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all() #Fetch all book instances from the database
+    context = {'book_list': books}
+    
+    return render(request, books/list_books.html , context)
+
