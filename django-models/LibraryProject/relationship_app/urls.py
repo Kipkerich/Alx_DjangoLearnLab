5ac376da02c07384d1c0fb0c5662c3fe0f9772d0
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-07-24 06:14:38 +0300

    added url file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from .views import list_books
 urlpatterns = [
     path('library-function/', views.library_view, name='function-view'),
     path('library-class/', views.LibraryDetailView.as_view(), name='class-view'),
