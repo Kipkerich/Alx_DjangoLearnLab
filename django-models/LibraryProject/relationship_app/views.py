6e62f8d099222bb16a710c4af57985e8d8eb77be
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-07-25 06:33:21 +0300

    added views and url for registration and login files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from .models import Library, Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 
-def library_list(request):
+def library_view(request):
     book = Book.objects.all() #Fetch all library instances from the database
     context = {'book_list': book}
     
-    return render(request, relationship_app/list_books.html , context)
+    return render(request, 'relationship_app/list_books.html' , context)
 
 class LibraryDetailView(DetailView):
     model = Library
@@ -15,6 +18,11 @@ class LibraryDetailView(DetailView):
     
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
+    
+class SignUpView():
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
         
         
     
