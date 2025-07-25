30196c97aee834d852e484d72c545a6a9140b96c
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-07-25 07:40:06 +0300

    added views and url for registration and login files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('library-function/', views.library_view, name='function-view'),
     path('library-class/', views.LibraryDetailView.as_view(), name='class-view'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(template_name=''), name='logout'),
+    path('register/', views.register, name='register'),
     
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Library, Book
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
@@ -19,10 +19,16 @@ class LibraryDetailView(DetailView):
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
     
-class SignUpView():
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'relationship_app/register.html'
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+            
+        return render(request, 'relationship_app/register.html', {'form': form})
         
         
     
