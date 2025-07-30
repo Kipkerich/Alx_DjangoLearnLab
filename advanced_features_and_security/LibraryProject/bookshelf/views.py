573b9c7f1f3850229735999bf29a8dcc50a73153
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-07-30 22:47:40 +0300

    added permissions on the views file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Permission
+from .models import Book
 
-# Create your views here.
+
+@Permission('bookshelf.can_edit', raise_exception=True)
+
+class BookAdmin(Book):
+    try:
+        book_list = ("title")
+        
+    except: 
+        PermissionError('Do not have permission to execute this command')
+    
+        
+    
+    
