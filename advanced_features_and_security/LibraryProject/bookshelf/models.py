669035290f8d5255a1d829c5c663bcab1563b46c
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-07-30 13:54:30 +0300

    added custom user class

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import BaseUserManager,AbstractUser
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication = models.IntegerField()
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
     profile_photo = models.ImageField()
