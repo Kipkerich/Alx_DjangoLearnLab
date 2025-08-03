3b83dde68710b67c97422cc339bb18be2be7e4c0
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-08-03 20:27:46 +0300

    added api views on the settings file

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+class MyAPIVIEW(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        #Only authenticated users can acess this view
+        return Response({'message': 'Hello, authenticated user!'})
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
