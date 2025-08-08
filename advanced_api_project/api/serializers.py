1f1001c3b9e5a2a6bcbf0dea175305898f84318c
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-08-08 23:39:16 +0300

    added advanced-api directory

---
+++ advanced_api_project/api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Book, Author
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name', 'publication_year']
+        
+    def validate(self, data):
+        if len(data['publication_year']) > 2025:
+            raise serializers.ValidationError("year cannot be in the future")
+        return data
