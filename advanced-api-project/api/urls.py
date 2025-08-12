33f12aa2b23271c89fe534140b215a3544b8c480
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-08-12 18:19:01 +0300

    updated the implementation of the api view and configuration of the urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns =[
     path('books/', DetailView, name='get_books'),
     path('books/create', CreateView, name='post_books'),
     path('books/update', UpdateView, name='put_book'),
-    path('books/delete', DeleteView, name='delete_book')
+    path('books/delete', DeleteView, name='delete_book'),
     path('books/<int:pk>', DetailView, name='book_details'),
 ]
